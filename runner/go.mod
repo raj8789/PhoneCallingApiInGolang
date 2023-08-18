94cbf3131bd109d4699a7143b6ee9862eba29585
raj8789 <raushanraj8789@gmail.com>
2023-08-18 08:03:20 +0530

    First Full Commit With Functionality like can Make Phone call through a test Number

---
+++ runner/go.mod
@@ -0,0 +1,16 @@
+module runner
+
+go 1.20
+
+require github.com/plivo/plivo-go/v7 v7.36.0
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v1.11.0 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+)
---
+++ runner/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/twilio/twilio-go"
+	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+	from := os.Getenv("TWILIO_FROM_PHONE_NUMBER")
+	to := os.Getenv("TWILIO_TO_PHONE_NUMBER")
+	accountSID := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	fmt.Println(from)
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSID,
+		Password: authToken,
+	})
+	params := &twilioApi.CreateCallParams{}
+	params.SetTo(to)
+	params.SetFrom(from)
+	params.SetUrl("http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
+
+	resp, err := client.Api.CreateCall(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Call Status: " + *resp.Status)
+		fmt.Println("Call Sid: " + *resp.Sid)
+		fmt.Println("Call Direction: " + *resp.Direction)
+	}
+}
+
+//Pushplatadevi12#
+
+//https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1
+
+//https://github.com/twilio/twilio-go
